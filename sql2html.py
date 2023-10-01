65a8103be701f2c0f699e480be2b529beb844648
beamzer <github@oiepoie.nl>
2023-10-01 20:28:00 +0200

    v2.1f

--- sql2html.py
+++ sql2html.py
@@ -8,7 +8,7 @@ import re
 from datetime import date
 
 # for this to display and work (sort) properly, sort.js and styles.css need to be in the directory above index.html
-version = "v2.1e, 20230817"
+version = "v2.1f, 20230908"
 
 html_table = ""
 
@@ -273,7 +273,7 @@ html_page = """
   <br />
   This overview is generated with: <a href="https://github.com/beamzer/ScirtScan">https://github.com/beamzer/ScirtScan</a><br /><br />
   Clicks on table headers will result in sorting or reverse sorting on that column content <br />
-  &#187; Click on the date field to see the details for that website<br />
+  &#187; Click on the check date in the far right column to see the detailed logs for that website<br />
   In the security.txt column clicks on green checkbox will show the contents of that URL <br />
   Clicking in the robots.txt column on the checkmarks will try to open that URL<br />
   Click here for a: <a href="website_checks.xlsx">Excel file with the contents of this table</a><br />
